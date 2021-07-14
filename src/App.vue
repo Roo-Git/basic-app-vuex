e8229a443aa3df538622812221f86e0b8d8f811e
Robert <robert@climatetrade.com>
2021-07-14 17:06:27 +0200

    my first global state

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <h1>{{ $store.state.titleApp }}</h1>
+  <h2>{{ titleApp }}</h2>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { useStore } from "vuex";
+export default {
+  name: "App",
+  setup() {
+    const store = useStore();
+    console.log(store.state.titleApp);
 
-#nav {
-  padding: 30px;
+    return {
+      ...store.state,
+    };
+  },
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,44 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    titleApp: "Memes",
+    memes: [],
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+  getters: {},
+});
+
+/*
+
+*** VUEX ***
+
+STORE - Donde guardamos todos los datos de nuestra APP.
+
+STATE - Datos de nuestra APP accesibles para ser renderizados desde cualquier componente.
+
+MUTATIONS - Funciones encargadas de actualizar el estado de nuestro Store cuando nosotros se lo indiquemos y lo necesitemos.
+
+ACTIONS - Parecidas a las Mutations. La diferencia es que en lugar de mutar un estado directamente, lo que hace es realizar un commit sobre una mutacion. Esto significa que puede ejecutar cualquier mutation y las actions pueden ser operaciones asíncronas.
+
+GETTERS - Son funciones que sirven para obtener cualquier estado.
+
+MODULES - Los modulos sirven para dividir nuestra STORE, en pequeños objetos pequeños (modulos). Los cuales tendrán sus propios states, mutations, actions e incluso sus propios modulos anidados.
+
+FLOW - Flujo de como trabaja Vuex el estado de nuestra APP.
+
+1. Se carga un componente.
+
+2. El componente dispachea una accion que llama a una API(Backend)
+
+3. Esa API devuelve los datos a la accion.
+
+4. La accion ejecuta un commit.
+
+5. El commit ejecuta un mutation que a su vez este mutation actualiza un estado.
+
+6. Este estado es recogido y renderizado por un componente.
+
+*/
