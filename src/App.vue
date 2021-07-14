5a69d603ba4d672b0628cc5ae7d2271843dec7b1
Robert <robert@climatetrade.com>
2021-07-14 17:16:53 +0200

    added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <template>
-  <h1>{{ $store.state.titleApp }}</h1>
-  <h2>{{ titleApp }}</h2>
+  <button type="button" class="btn btn-primary">Primary</button>
 </template>
 
 <script>
-import { useStore } from "vuex";
-export default {
-  name: "App",
-  setup() {
-    const store = useStore();
-    console.log(store.state.titleApp);
-
-    return {
-      ...store.state,
-    };
-  },
-};
+export default {};
 </script>
 
 <style lang="scss" scoped></style>
